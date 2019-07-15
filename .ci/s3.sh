8516374c05b9c466a5a7d90cc1fa8558d76af94c
Caian R. Ertl <hi@caian.org>
2019-07-15 19:47:37 -0300

    ci: configure the S3 bucket to host the documentation static files

--- .ci/s3.sh
+++ .ci/s3.sh
@@ -22,3 +22,6 @@ aws s3 rm --recursive "$s3Bucket"
 
 # upload all files to the bucket
 aws s3 cp --recursive ./docs "$s3Bucket"
+
+# configure the bucket to host a static website
+aws s3 website "$s3Bucket" --index-document index.html
