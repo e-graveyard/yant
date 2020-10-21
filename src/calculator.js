4d97dbd66358b37dafa2c9391f48146cf531f413
Caian R. Ertl <hi@caian.org>
2020-10-21 04:23:33 -0300

    style: run prettier & standard

--- src/calculator.js
+++ src/calculator.js
@@ -8,63 +8,63 @@
  */
 export class Calculator {
   /**
-     * Creates a new calculator.
-     *
-     * @param {number} operand - First operand.
-     */
+   * Creates a new calculator.
+   *
+   * @param {number} operand - First operand.
+   */
   constructor (operand) {
     this.result = operand
   }
 
   /**
-     * Adds a value to the current result.
-     *
-     * @param {number} value - The number value to be added.
-     * @returns {object} - The class itself object.
-     */
+   * Adds a value to the current result.
+   *
+   * @param {number} value - The number value to be added.
+   * @returns {object} - The class itself object.
+   */
   plus (value) {
     this.result += value
     return this
   }
 
   /**
-     * Subtracts a value to the current result.
-     *
-     * @param {number} value - The number value to be subtracted.
-     * @returns {object} - The class itself object.
-     */
+   * Subtracts a value to the current result.
+   *
+   * @param {number} value - The number value to be subtracted.
+   * @returns {object} - The class itself object.
+   */
   minus (value) {
     this.result -= value
     return this
   }
 
   /**
-     * Divides the result by a given value.
-     *
-     * @param {number} value - The number to divide the result.
-     * @returns {object} - The class itself object.
-     */
+   * Divides the result by a given value.
+   *
+   * @param {number} value - The number to divide the result.
+   * @returns {object} - The class itself object.
+   */
   divided (value) {
     this.result /= value
     return this
   }
 
   /**
-     * Multiplies the result by a given value.
-     *
-     * @param {number} value - The number to multiply the result.
-     * @returns {object} - The class itself object.
-     */
+   * Multiplies the result by a given value.
+   *
+   * @param {number} value - The number to multiply the result.
+   * @returns {object} - The class itself object.
+   */
   times (value) {
     this.result *= value
     return this
   }
 
   /**
-     * Finishes the calculation and returns the final result.
-     *
-     * @returns {number}
-     */
+   * Finishes the calculation and returns the final result.
+   *
+   * @returns {number}
+   */
   equals () {
     return this.result
   }
