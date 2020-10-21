3377c14e3c71e75ae3e2f758b9d60a5841442e21
Caian R. Ertl <hi@caian.org>
2020-10-21 04:05:30 -0300

    chore: add babel configuration file

---
+++ .babelrc.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    /* module-resolver */
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: [path.resolve('./')],
+        alias: {
+          '@yant': './src',
+        }
+      },
+    ]
+    /* end */
+  ],
+}
