3dd5fe27a50a1fecbddacc4c8357bb66c9220d91
Caian R. Ertl <hi@caian.org>
2020-10-21 04:23:33 -0300

    style: run prettier & standard

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ function multiplicationTable (operand) {
   console.log(`Times table of ${operand}:\n`)
 
   for (let i = 1; i <= 10; i++) {
-    const result = new Calculator(i)
-      .times(operand)
-      .equals()
+    const result = new Calculator(i).times(operand).equals()
 
     console.log(`${i} x ${operand} = ${result}`)
   }
