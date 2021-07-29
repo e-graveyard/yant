9a28137ee882c701c011151397ecdd8f51de567b
Caian R. Ertl <hi@caian.org>
2021-07-28 21:42:46 -0300

    feat: repeat function to run many multiplication tables

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import { Calculator } from './calculator'
 
 /**
+ * Repeats a given callback function N times.
+ *
+ * @param {number} n - the amount of times to repeat.
+ * @param {number} cb - the callback function.
+ */
+const each = (n, cb) => Array.from(Array(n).keys()).forEach(cb)
+
+/**
  * Calculate and outputs a multiplication table of a given value.
  *
  * @param {number} operand - the multiplication number.
  */
 function multiplicationTable (operand) {
-  console.log(`Times table of ${operand}:\n`)
+  console.log(`\nTimes table of ${operand}:\n`)
 
-  for (let i = 1; i <= 10; i++) {
-    const result = new Calculator(i).times(operand).equals()
+  each(10, (_, i) => {
+    const iter = i + 1
+    const result = new Calculator(iter).times(operand).equals()
 
-    console.log(`${i} x ${operand} = ${result}`)
-  }
+    console.log(`${iter} x ${operand} = ${result}`)
+  })
 }
 
-multiplicationTable(7)
+each(10, (_, i) => multiplicationTable(i + 1))
